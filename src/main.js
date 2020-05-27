623319754fbdcd533c22b57947b453998759003c
MathBoulot <mathieu.le.bellec@gmail.com>
2020-05-27 17:16:13 +0200

    Feat(config): Configuration de base

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Router from './router';
+import Store from './store/store';
+import Vuelidate from 'vuelidate';
 
-Vue.config.productionTip = false
+window.$ = require('jquery');
+window.JQuery = require('jquery');
+
+Vue.config.productionTip = false;
+
+Vue.use(Vuelidate);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router: Router,
+  store: Store,
+  render: (h) => h(App),
+}).$mount('#app');
---
+++ src/router.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Main from './App.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    component: Main,
+  },
+  { path: '*', redirect: '/' },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes,
+});
+
+export default router;
