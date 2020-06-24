be5d2edb085a36d5c3ca2cafa078716da3614838
MathBoulot <mathieu.le.bellec@gmail.com>
2020-06-24 09:56:42 +0200

    feat(footer): design

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,14 +1,23 @@
 <template src="./footer.html"></template>
 
 <script>
-import Cat from './../svg/svg-cat.vue';
-import TiChat from './../svg/svg-tiChat29.vue';
-
+import Cat from "./../svg/svg-cat.vue";
+import TiChat from "./../svg/svg-tiChat29.vue";
+import SvgPhone from "./../svg/svg-phone";
+import SvgEmail from "./../svg/svg-email";
+import SvgAddress from "./../svg/svg-address";
+import SvgFacebook from "./../svg/svg-facebook";
+import SvgInstagram from "./../svg/svg-instagram";
 export default {
-    components: {
-        Cat,
-        TiChat
-    },
+  components: {
+    SvgPhone,
+    SvgEmail,
+    SvgAddress,
+    Cat,
+    SvgFacebook,
+    SvgInstagram,
+    TiChat
+  }
 };
 </script>
 
