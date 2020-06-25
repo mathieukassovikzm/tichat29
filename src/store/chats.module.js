26e6d442bc1bbf898dbf232b8c72be89eaea0dfb
MathBoulot <mathieu.le.bellec@gmail.com>
2020-06-25 11:04:37 +0200

    feat(store): Création de tous les fichiers

---
+++ src/store/chats.module.js
@@ -0,0 +1,13 @@
+const state = {};
+const getters = {};
+const mutations = {};
+const actions = {};
+
+export default {
+  namespaced: true,
+  strict: true,
+  state: state,
+  getters: getters,
+  mutations: mutations,
+  actions: actions,
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import MainStore from './store.module.js';
+import ChatsStore from './chats.module.js';
+import ArticlesStore from './articles.module.js';
+import EventsStore from './events.module.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     mainStore: MainStore,
+    chatsStore: ChatsStore,
+    articlesStore: ArticlesStore,
+    eventsStore: EventsStore,
   },
 });
