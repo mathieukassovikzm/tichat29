63085deeda7b707ab3fbd452297e406a419ec73c
MathBoulot <mathieu.le.bellec@gmail.com>
2020-07-02 08:20:51 +0200

    feat(json): setup

--- src/store/chats.module.js
+++ src/store/chats.module.js
@@ -1,7 +1,38 @@
-const state = {};
+import dataChats from './data/chats.json';
+
+const state = {
+  listChats: [],
+  countChats: 0,
+};
 const getters = {};
-const mutations = {};
-const actions = {};
+const mutations = {
+  SETLISTCHATS: (state, data) => {
+    state.listChats = data;
+  },
+};
+const actions = {
+  fetchListChats(store) {
+    try {
+      let data = dataChats;
+      if (data) {
+        let list = [];
+        for (const [key, value] of Object.entries(data)) {
+          let chat = {
+            id: key,
+            name: value.name,
+            desc: value.description,
+            photos: value.photos,
+            adopt: value.adopte,
+          };
+          list.push(chat);
+        }
+        store.commit('SETLISTCHATS', list);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 
 export default {
   namespaced: true,
