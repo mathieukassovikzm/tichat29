ac13b1395d458541883570dfcbbbe65a968363d2
MathBoulot <mathieu.le.bellec@gmail.com>
2020-07-17 16:52:26 +0200

    fix(miniDiapo): loading des photos part2

--- src/components/mini-diapo/mini-diapo.vue
+++ src/components/mini-diapo/mini-diapo.vue
@@ -5,10 +5,38 @@ export default {
   props: ["listPhotos"],
   data() {
     return {
-      index: 0
+      index: 0,
+      nbThumbnails: 2
     };
   },
-  methods: {}
+  computed: {
+    getMainPhoto() {
+      return {
+        ...this.listPhotos[this.index],
+        img:
+          this.listPhotos[this.index] &&
+          require(`@/assets/Chats/${this.listPhotos[this.index]}`)
+      };
+    },
+    getFilteredList() {
+      var newArray = this.listPhotos.filter(
+        el => el !== this.listPhotos[this.index]
+      );
+
+      console.log(newArray);
+      return newArray;
+    }
+  },
+  methods: {
+    getThumbnailPhoto(nbTN) {
+      return {
+        ...this.listPhotos[this.index + nbTN],
+        img:
+          this.listPhotos[this.index + nbTN] &&
+          require(`@/assets/Chats/${this.listPhotos[this.index + nbTN]}`)
+      };
+    }
+  }
 };
 </script>
 
