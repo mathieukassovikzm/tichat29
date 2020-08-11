2ec97083b141edce2ad4f3f9fbcecdf94a3e611f
Math <mathieu.le.bellec@gmail.com>
2020-08-11 21:00:54 +0200

    feat(HomePage): Style

--- src/pages/page-home/page-home.vue
+++ src/pages/page-home/page-home.vue
@@ -1,12 +1,13 @@
 <template src="./page-home.html"></template>
 
 <script>
-import Cat from "@/components/svg/svg-cat.vue";
-import TiChat from "@/components/svg/svg-tiChat29.vue";
-import PresAsso from "@/components/presentation-asso/presentation-asso.vue";
-import Events from "@/components/events/events.vue";
-import LastActu from "@/components/last-actu/last-actu.vue";
-import Help from "@/components/help/help.vue";
+import Cat from '@/components/svg/svg-cat.vue';
+import CatPaw from '@/components/svg/svg-cat-paw.vue';
+import TiChat from '@/components/svg/svg-tiChat29.vue';
+import PresAsso from '@/components/presentation-asso/presentation-asso.vue';
+import Events from '@/components/events/events.vue';
+import LastActu from '@/components/last-actu/last-actu.vue';
+import Help from '@/components/help/help.vue';
 
 export default {
   components: {
@@ -15,8 +16,9 @@ export default {
     PresAsso,
     Events,
     LastActu,
-    Help
-  }
+    Help,
+    CatPaw,
+  },
 };
 </script>
 
