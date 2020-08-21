9e9452fe3220c295e7529543523137ffa8b4583e
Math <mathieu.le.bellec@gmail.com>
2020-08-21 20:34:24 +0200

    feat(Menu): style

--- src/components/menu/menu.vue
+++ src/components/menu/menu.vue
@@ -1,17 +1,21 @@
 <template src="./menu.html"></template>
 
 <script>
-import Burger from "@/components/burger/burger.vue";
-import SvgCat from "@/components/svg/svg-cat.vue";
-import SvgTiChat from "@/components/svg/svg-tiChat29.vue";
-import SvgPhone from "@/components/svg/svg-phone.vue";
-import SvgFacebook from "@/components/svg/svg-facebook.vue";
-import SvgInstagram from "@/components/svg/svg-instagram.vue";
-import { mapGetters, mapActions } from "vuex";
-import router from "./../../router";
+import Burger from '@/components/burger/burger.vue';
+import SvgCat from '@/components/svg/svg-cat.vue';
+import SvgTiChat from '@/components/svg/svg-tiChat29.vue';
+import SvgPhone from '@/components/svg/svg-phone.vue';
+import SvgFacebook from '@/components/svg/svg-facebook.vue';
+import SvgInstagram from '@/components/svg/svg-instagram.vue';
+import SvgHome from '@/components/svg/svg-home.vue';
+import SvgCatPaw from '@/components/svg/svg-cat-paw-with-circle.vue';
+import SvgHeart from '@/components/svg/svg-heart.vue';
+import SvgScheduler from '@/components/svg/svg-scheduler.vue';
+import { mapGetters, mapActions } from 'vuex';
+import router from './../../router';
 
 export default {
-  name: "menuResp",
+  name: 'menuResp',
   components: {
     Burger,
     SvgTiChat,
@@ -19,32 +23,36 @@ export default {
     SvgPhone,
     SvgFacebook,
     SvgInstagram,
+    SvgHome,
+    SvgHeart,
+    SvgScheduler,
+    SvgCatPaw,
   },
   data() {
     return {
       navItems: [
-        { name: "Home", href: "PageHome" },
-        { name: "Nos chats", href: "NosChats" },
-        { name: "Actualitées", href: "PageActualitees" },
-        { name: "Devenir Famille d'acceuil", href: "PageFamilleDaccueil" },
-        { name: "Contact", href: "PageContact" },
+        { name: 'Home', href: 'PageHome' },
+        { name: 'Nos chats', href: 'NosChats' },
+        { name: 'Actualitées', href: 'PageActualitees' },
+        { name: "Devenir Famille d'acceuil", href: 'PageFamilleDaccueil' },
+        { name: 'Contact', href: 'PageContact' },
       ],
     };
   },
   computed: {
-    ...mapGetters("mainStore", {
-      getNavOpenState: "getNavOpenStateInStore",
+    ...mapGetters('mainStore', {
+      getNavOpenState: 'getNavOpenStateInStore',
     }),
 
     isOpen() {
-      return this.getNavOpenState === true ? "is-opened" : "is-closed";
+      return this.getNavOpenState === true ? 'is-opened' : 'is-closed';
     },
   },
   methods: {
-    ...mapActions("mainStore", {
-      toggleNavOpen: "toggleNavOpenInStore",
-      setToFalseNavOpen: "setToFalseNavOpenInStore",
-      setToTrueNavOpen: "setToTrueNavOpenInStore",
+    ...mapActions('mainStore', {
+      toggleNavOpen: 'toggleNavOpenInStore',
+      setToFalseNavOpen: 'setToFalseNavOpenInStore',
+      setToTrueNavOpen: 'setToTrueNavOpenInStore',
     }),
 
     linkClicked(idLinkClicked) {
