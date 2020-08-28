accf09e4079a21b6a4f8931314e55f0f67aa0402
Math <mathieu.le.bellec@gmail.com>
2020-08-28 19:38:25 +0200

    feat(page-contact): premier dev

--- src/pages/page-contact/page-contact.vue
+++ src/pages/page-contact/page-contact.vue
@@ -1,11 +1,96 @@
 <template src="./page-contact.html"></template>
 
 <script>
-import PageTitle from "./../../components/title/title.vue";
+import TitleH1 from "./../../components/title/title.vue";
+import SvgPhone from "@/components/svg/svg-phone";
+import SvgEmail from "@/components/svg/svg-email";
+import SvgAddress from "@/components/svg/svg-address";
+import SvgFacebook from "@/components/svg/svg-facebook";
+import SvgInstagram from "@/components/svg/svg-instagram";
+import { required } from "vuelidate/lib/validators";
 
 export default {
   components: {
-    PageTitle,
+    TitleH1,
+    SvgPhone,
+    SvgEmail,
+    SvgAddress,
+    SvgFacebook,
+    SvgInstagram,
+  },
+  data() {
+    return {
+      form: {
+        email: "",
+        firstname: "",
+        message: "",
+      },
+      classFormError: "form-error",
+      classInputValid: "valid",
+      errors: false,
+      formTouched: false,
+    };
+  },
+  /* List of requierement for each input */
+  validations: {
+    form: {
+      email: { required },
+      firstname: { required },
+      message: { required },
+    },
+  },
+  computed: {
+    formEmailValid() {
+      return !this.$v.form.email.$invalid ? this.classInputValid : "";
+    },
+    formFirstnameValid() {
+      return !this.$v.form.firstname.$invalid ? this.classInputValid : "";
+    },
+    formMessageValid() {
+      return !this.$v.form.message.$invalid ? this.classInputValid : "";
+    },
+    // v-show nameError if the form has been touch and if there is and error
+    formEmailError() {
+      return this.formTouched && this.$v.form.email.$error
+        ? this.classFormError
+        : "";
+    },
+    // v-show nameError if the form has been touch and if there is and error
+    formFirstnameError() {
+      return this.formTouched && this.$v.form.firstname.$error
+        ? this.classFormError
+        : "";
+    },
+    // v-show nameError if the form has been touch and if there is and error
+    formMessageError() {
+      return this.formTouched && this.$v.form.message.$error
+        ? this.classFormError
+        : "";
+    },
+  },
+  methods: {
+    /* Handle the user's submit
+     * @emit the event 'addRequestToChat' for his parent
+     */
+    onSubmit() {
+      this.formTouched = this.$v.form.$anyDirty;
+      this.errors = this.$v.form.$anyError;
+      if (this.errors == false && this.formTouched == true) {
+        let request = {
+          name: this.form.name,
+          query: this.form.query,
+          date: new Date(),
+        };
+        this.$emit("addRequestToChat", request);
+        this.cleanForm();
+      } else return;
+    },
+
+    /* Clean the form */
+    cleanForm() {
+      this.name = "";
+      this.query = "";
+    },
   },
 };
 </script>
