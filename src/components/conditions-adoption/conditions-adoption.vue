da475e2f72250568a163ef69b26f4bb8448a85b1
MathBoulot <mathieu.le.bellec@gmail.com>
2020-10-07 09:20:30 +0200

    Feat: Ajout de text sur différentes pages

--- src/components/conditions-adoption/conditions-adoption.vue
+++ src/components/conditions-adoption/conditions-adoption.vue
@@ -3,8 +3,47 @@
     <section class="section-container">
       <PageTitle class="bkg-light">Conditions</PageTitle>
       <div class="conditions">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae cupiditate similique et quaerat eaque harum commodi? Autem quia molestias recusandae similique, velit sapiente neque dolor amet nobis doloremque quis accusantium?</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae cupiditate similique et quaerat eaque harum commodi? Autem quia molestias recusandae similique, velit sapiente neque dolor amet nobis doloremque quis accusantium?</p>
+        <p>
+          Dans un premier temps, la prise de contact se fait auprès d'un des
+          responsables de l'association, via téléphone, mail ou facebook. Nous
+          connaissons bien nos chats, aussi des questions vous seront posées
+          afin de déterminer si le caractère et le mode de vie de celui-ci peut
+          vous correspondre.
+        </p>
+        <p>
+          Dans un second temps, nous vous mettrons en contact avec la famille
+          d'accueil de l'animal en question, afin de convenir d'un rendez-vous
+          pour le rencontrer. Toute adoption donne lieu à un contrat auprès de
+          l'association et à un engagement de stérilisation s'il s'agit d'un
+          chaton.
+        </p>
+        <p>Tarifs d'adoption</p>
+        <p>
+          Chats adultes : 120€ Comprenant la stérilisation/castration,
+          l'identification, la primo-vaccination (et rappel le cas échéant), et
+          le déparasitage interne/externe.
+        </p>
+        <p>
+          Chatons : 120€ Comprenant la stérilisation/castration,
+          l'identification, la primo-vaccination (et rappel le cas échéant), et
+          le déparasitage interne/externe. La stérilisation est obligatoire à
+          l'âge de 6 mois, les adoptants rempliront un engagement de
+          stérilisation lors de l'adoption et fourniront un chèque de caution de
+          120euros, qui sera détruit lors de la réception de l'attestation
+          vétérinaire.
+        </p>
+        <p>
+          Chats en "sauvetage" : chats et chatons, malades, âgés, handicapés...
+          : don libre (+ chèque de caution pour la stérilisation future s'il
+          s'agit d'un chaton)
+        </p>
+        <p>
+          Sachez que ces frais sont inférieurs à ceux que vous paierez en tant
+          que particulier chez un vétérinaire, et qu'ils vous donneront la
+          garantie d'un animal en règle (l'identification étant obligatoire en
+          France). De plus, la stérilisation est le seul moyen de lutter contre
+          la prolifération des futures petites misères de demain.
+        </p>
       </div>
     </section>
   </div>
--- src/sections/presentation-asso/presentation-asso.vue
+++ src/sections/presentation-asso/presentation-asso.vue
@@ -9,7 +9,28 @@
         abandonnés. L'objectif étant de recueillir, identifier, stériliser et
         placer les chats abandonnés.
       </p>
+      <p>Nous militons pour des adoptions responsables et engagées.</p>
+      <p>
+        Nos protégés, chats jeunes et moins jeunes, ont tous un point commun:
+        avoir souffert de l'abandon, souvent de l'errance et de conditions de
+        vie misérable dans la rue.
+      </p>
+      <p>
+        Nous tenons à responsabiliser les adoptants potentiels sur le fait
+        qu'adopter un animal est bien un engagement sur un certain nombre
+        d'années : ce n'est ni une peluche, ni un jouet !
+      </p>
+      <p>
+        De part leur histoire, certains de nos chats auront besoin de patience
+        et de douceur pour reprendre confiance en l'humain, qui les a malmenés.
+      </p>
+      <p>
+        En adoptant un de nos protégés, vous nous permettez de sauver une
+        nouvelle vie!
+      </p>
+      <p></p>
     </div>
+
     <SectionBkgImg class="img-obj-asso bkg-light">
       <div class="section-bkg-img img-obj-asso">
         <div class="section-container">
@@ -17,10 +38,10 @@
           <p>
             Les chats sociables sont recueillis par notre association pour être
             proposés à l’nos-chats (ils sont identifiés, vaccinés, stérilisés ou
-            castrés). Afin de remédier à la prolifération des chats errants qui ne
-            sont pas sociables, nous souhaitons les faire stériliser ou castrer par un
-            vétérinaire, puis les remettre sur site où ils sont nourris et disposent
-            d’un abri que nous confectionnons.
+            castrés). Afin de remédier à la prolifération des chats errants qui
+            ne sont pas sociables, nous souhaitons les faire stériliser ou
+            castrer par un vétérinaire, puis les remettre sur site où ils sont
+            nourris et disposent d’un abri que nous confectionnons.
           </p>
         </div>
       </div>
@@ -29,23 +50,23 @@
     <div class="section-container">
       <TitleH1 class="bkg-light">Familles d'accueil</TitleH1>
       <p>
-        Nous sommes à la recherche de familles d'accueil afin de mettre nos petits
-        protégés à l'abri le temps de leur nos-chats. Les chats sont mis en
-        quarantaine dans un premier temps, soit dans la famille d'accueil ou soit
-        dans une famille d'accueil prévue pour les quarantaines. Ils seront vus
-        par un vétérinaire, déparasités interne et externe, identifiés, stérilisés
-        et primo vaccinés.
+        Nous sommes à la recherche de familles d'accueil afin de mettre nos
+        petits protégés à l'abri le temps de leur nos-chats. Les chats sont mis
+        en quarantaine dans un premier temps, soit dans la famille d'accueil ou
+        soit dans une famille d'accueil prévue pour les quarantaines. Ils seront
+        vus par un vétérinaire, déparasités interne et externe, identifiés,
+        stérilisés et primo vaccinés.
       </p>
     </div>
     <SectionBkgImg class="img-fam-acc bkg-light">
       <div class="section-container">
         <p>
-          Ti Chat 29 fournit les croquettes et prend en charge
-          les frais vétérinaires. Actuellement les demandes de prise en charge
-          affluent mais nous manquons cruellement de familles d'accueil. Etre
-          famille d'accueil c'est de l'amour et le bonheur de participer à un
-          sauvetage : n'hésitez pas à en parler autour de vous ! Nous avons besoin
-          de votre aide pour donner une seconde chance à tous ces chats abandonnés.
+          Ti Chat 29 fournit les croquettes et prend en charge les frais
+          vétérinaires. Actuellement les demandes de prise en charge affluent
+          mais nous manquons cruellement de familles d'accueil. Etre famille
+          d'accueil c'est de l'amour et le bonheur de participer à un sauvetage
+          : n'hésitez pas à en parler autour de vous ! Nous avons besoin de
+          votre aide pour donner une seconde chance à tous ces chats abandonnés.
         </p>
       </div>
     </SectionBkgImg>
