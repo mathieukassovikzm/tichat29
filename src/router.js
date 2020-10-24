76cfff49e8401a51aaf0f2e5ee298fc576160f45
Math <mathieu.le.bellec@gmail.com>
2020-10-24 20:30:09 +0200

    fix(router): scroll to top when navigate

--- src/router.js
+++ src/router.js
@@ -68,6 +68,9 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   routes: routes,
+  scrollBehavior() {
+    document.getElementById('app').scrollIntoView();
+  }
 });
 
 export default router;
